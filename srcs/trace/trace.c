4e8251faf1b4a654b19d610d8bde14e15836a358
dkglee <ideade9@gmail.com>
2021-03-23 18:16:43 +0900

    init trace

--- srcs/trace/trace.c
+++ srcs/trace/trace.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render.c                                           :+:      :+:    :+:   */
+/*   trace.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: deulee <deulee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/21 16:47:42 by deulee            #+#    #+#             */
-/*   Updated: 2021/03/23 18:13:34 by deulee           ###   ########.fr       */
+/*   Created: 2021/03/23 18:13:54 by deulee            #+#    #+#             */
+/*   Updated: 2021/03/23 18:15:16 by deulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void		init_render(t_render *render)
+void	init_trace(t_trace *trace)
 {
-	init_info(&render->stuff);
-	init_control(&render->control);
-	init_trace(&render->trace);
+	trace->width = 0;
+	trace->height = 0;
+	trace->mlx = NULL;
+	trace->mlx_win = NULL;
+	trace->img = NULL;
+	trace->addr = NULL;
 }
