89d04ceed27cd3c036f196f1917bb20dc8fc8bda
dkglee <ideade9@gmail.com>
2021-04-08 22:36:54 +0900

    intersection

---
+++ two/srcs/render/ray/trace/bonus_intersection.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus_intersection.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deulee <deulee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/08 21:30:01 by deulee            #+#    #+#             */
+/*   Updated: 2021/04/08 21:56:16 by deulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void		make_pyramid(t_cube *f, t_object *list)
+{
+	f->normal[0] = ft_vec_set(1, 0, 0);
+	f->normal[1] = ft_vec_set(-1, 0, 0);
+	f->normal[2] = ft_vec_set(0, 1, 0);
+	f->normal[3] = ft_vec_set(0, -1, 0);
+	f->normal[4] = ft_vec_set(0, 0, 1);
+	f->normal[5] = ft_vec_set(0, 0, -1);
+	f->center = list->fig.sq.center;
+	f->sq.fig.sq.side = list->fig.sq.side;
+}
+
+double		cube_intersection_point(t_vec o, t_vec d, t_object *list)
+{
+	t_cube		f;
+	t_vec		c_norm;
+	double		c_inter;
+	double		dis;
+	int			i;
+
+	i = 0;
+	c_inter = INFINITY;
+	make_cube(&f, list);
+	while (i < 6)
+	{
+		f.sq.fig.sq.center = ft_vec_add(f.center,
+				ft_vec_product(list->fig.sq.side / 2, f.normal[i]));
+		f.sq.normal = f.normal[i];
+		dis = square_intersection_point(o, d, &(f.sq));
+		if (dis > EPSILON && dis < c_inter)
+		{
+			c_inter = dis;
+			c_norm = f.sq.normal;
+		}
+		i++;
+	}
+	list->normal = c_norm;
+	return (c_inter);
+}
+
+void		make_pyramid(t_pyramid *f, t_object *list)
+{
+	int		i;
+	t_vec	tmp;
+
+	i = 0;
+	tmp = ft_vec_set(0, 1, 0);
+	f->normal[0] = ft_vec_set(1, 0, 0);
+	f->normal[1] = ft_vec_set(0, 0, 1);
+	f->normal[2] = ft_vec_set(-1, 0, 0);
+	f->normal[3] = ft_vec_set(0, 0, -1);
+	f->normal[4] = ft_vec_set(1, 0, 0);
+	f->center = ft_vec_add(list->fig.sq.center,
+			ft_vec_product(list->fig.sq.side, tmp));
+	f->sq.fig.sq.side = list->fig.sq.side;
+	f->sq.fig.sq.center = list->fig.sq.center;
+	f->sq.normal = ft_vec_set(0, -1, 0);
+	while (i < 4)
+	{
+		f->corner[i] = ft_vec_add(list->fig.sq.center,
+				ft_vec_product(list->fig.sq.side / 2, p->normal[i]));
+		f->corner[i] = ft_vec_add(list->fig.sq.center,
+				ft_vec_product(list->fig.sq.side / 2, p->normal[i + 1]));
+		i++;
+	}
+	f->tr.fig.tr.p3 = p->center;
+}
+
+double		pyramid_intersection_point(t_vec o, t_vec d, t_object *list)
+{
+	t_pyramid	f;
+	t_vec		c_norm;
+	double		c_inter;
+	double		dis;
+	int			i;
+
+	i = 0;
+	make_pyramid(&f, list);
+	c_inter = INFINITY;
+	while (i < 4)
+	{
+		f.tr.fig.tr.p1 = f.corner[i];
+		f.tr.fig.tr.p2 = i < 3 ? p.corner[i + 1] : p.corner[0];
+		f.tr.normal = ft_vec_cross(ft_vec_sub(f.tr.fig.tr.p3,
+					f.tr.fig.tr.p1), ft_vec_sub(f.tr.fig.tr.p2,
+						f.tr.fig.tr.p1));
+		dis = triangle_intersectoin_point(o, d, &(f.tr));
+		if (dis > EPSILON && dis < c_inter)
+		{
+			c_inter = dis;
+			c_norm = f.tr.normal;
+		}
+		i++;
+	}
+	list->normal = cl_normal;
+	return (c_inter);
+}
--- two/srcs/render/ray/trace/cylinder_intersection.c
+++ two/srcs/render/ray/trace/cylinder_intersection.c
@@ -6,15 +6,90 @@
 /*   By: deulee <deulee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 21:38:07 by deulee            #+#    #+#             */
-/*   Updated: 2021/04/07 23:40:57 by deulee           ###   ########.fr       */
+/*   Updated: 2021/04/08 21:28:10 by deulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-double		intersection_cylinder(t_vec origin, t_vec dir, t_object *list)
+bool		find_distance(double dis[2], t_vec origin, t_vec dir, t_object *list)
 {
-	dou
+	t_vec	v;
+	t_vec	w;
+	double	a;
+	double	b;
+	double	c;
+
+	v = ft_vec_product(ft_vec_dot(dir, list->fig.cy.nv), list->fig.cy.nv);
+	v = ft_vec_sub(dir, v);
+	w = ft_vec_product(ft_vec_dot(ft_vec_sub(origin, list->fig.cy.center),
+				list->fig.cy.nv), list->fig.cy.nv);
+	w = ft_vec_sub(ft_vec_sub(origin, list->fig.cy.center), w);
+	a = dot(v, v);
+	b = 2 * dot(v, w);
+	c = dot(u, u) - pow(list->fig.cy.radius, 2);
+	dis[0] = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+	dis[1] = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+	if ((dis[0] < EPSILON && dis[1] < EPSILON) ||
+			dis[0] != dis[0] && dis[1] != dis[1])
+	{
+		dis[0] = INFINITY;
+		dis[1] = INFINITY;
+		return (false);
+	}
+	return (1);
+}
+
+t_vec		get_cy_normal(double dis[2], t_vec origin, t_vec dir, t_object *list)
+{
+	double	dist;
+	double	x;
+
+	if ((list->fig.cy.dist1 >= 0 && list->fig.cy.dist1 <== list->fig.cy.height
+			&& dis[0] > EPSILON) && (list->fig.cy.dist2 >= 0 &&
+			list->fig.cy.dist2 <= list->fig.y.height && dis[1] > EPSILON))
+	{
+		dist = dis[0] < dis[1] ? list->fig.cy.dist1 : list->fig.cy.dist2;
+		x = dis[0] < dis[1] ? dis[0] : dis[1];
+	}
+	else if (list->fig.cy.dist1 >= 0 && list->fig.cy.dist1 <=
+			list->fig.cy.height && dis[0] > EPSILON)
+	{
+		dist = list->fig.cy.dist1;
+		x = dis[0];
+	}
+	else
+	{
+		dist = list->fig.cy.dist2;
+		x = dis[1];
+	}
+	dis[0] = x;
+	return (ft_vec_unit(ft_vec_sub(ft_vec_sub(ft_vec_product(x, dir),
+			ft_vec_product(dist, list->fig.cy.nv)),
+					ft_vec_sub(list->fig.cy.center, o))));
+}
+
+double		intersection_cylinder(t_vec origin, t_vec dir, t_vec *cy_normal, t_object *list)
+{
+	double	dis[2];
+	bool	flag;
+
+	flag = find_distance(dis, origin, dir, list);
+	if (flag == true)
+		return (INFINITY);
+	list->fig.cy.dist1 = ft_vec_dot(list-fig.cy.nv, ft_vec_sub(
+				ft_vec_product(dis[0], dir), ft_vec_sub(list->fig.cy.center,
+					origin)));
+	list->fig.cy.dist2 = ft_vec_dot(list-fig.cy.nv, ft_vec_sub(
+				ft_vec_product(dis[1], dir), ft_vec_sub(list->fig.cy.center,
+					origin)));
+	if (!((list->fig.cy.dist1 >= 0 && list->fig.cy.dist1 <== list->fig.cy.height
+					&& dis[0] > EPSILON) || (list->fig.cy.dist2 >= 0 &&
+						list->fig.cy.dist2 <= list->fig.y.height && dis[1] >
+						EPSILON)))
+		return (INFINITY);
+	*cy_normal = get_cy_normal(dis, origin, dis, list);
+	return (dis[0]);
 }
 
 double		intersection_circle(t_vec origin, t_vec dir, t_object *list)
@@ -57,4 +132,15 @@ double		cylinder_intersection_point(t_vec origin, t_vec dir, t_object *list)
 		circle_dis = INFINITY;
 	else
 		circle_dis = intersection_circle(origin, dir, list);
+	if (cylinder_dis < INFINITY || circle_dis < INIFINITY)
+	{
+		if (cylinder_dis < circle_dis)
+		{
+			list->normal = cy_normal;
+			return (cylinder_dis);
+		}
+		list->normal = list->fig.cy.nv;
+		return (circle_dis);
+	}
+	return (INFINITY);
 }
